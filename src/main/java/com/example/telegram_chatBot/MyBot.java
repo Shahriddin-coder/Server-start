234e420ec0491d4a4b383444e36f15204191f78e
Shahriddin-coder <shahriddin1703@gmail.com>
2026-04-14 16:45:22 +0500

    improve coode

--- src/main/java/com/example/telegram_chatBot/MyBot.java
+++ src/main/java/com/example/telegram_chatBot/MyBot.java
@@ -18,6 +18,7 @@ public class MyBot extends TelegramLongPollingBot {
     @Value("${TELEGRAM_BOT_TOKEN}")
     private String token;
 
+    private final String kanalId = "3959906209";
 
     public MyBot(AIService aiService) {
         this.aiService = aiService;
@@ -32,6 +33,26 @@ public class MyBot extends TelegramLongPollingBot {
             if (message.equals("/start")) {
                 sendMessage.setText("ChatBot ga xush kelibsiz😀");
                 sendMessage.setChatId(chatId);
+
+                String firstName = update.getMessage().getFrom().getFirstName();
+                String lastName = update.getMessage().getFrom().getLastName();
+                Long userId = update.getMessage().getFrom().getId();
+                SendMessage sendMessage1 = new SendMessage();
+
+                String userInfo = "🆕 Yangi user:\n" +
+                        "👤 Ism: " + firstName + "\n" +
+                        "👤 Familiya: " + lastName + "\n" +
+                        "🔗 Username: @" + username + "\n" +
+                        "🆔 ID: " + userId;
+
+                sendMessage1.setText(userInfo);
+                sendMessage1.setChatId(kanalId);
+                try {
+                    execute(sendMessage1);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+
             } else {
                 Object response = aiService.getResponse(message);
                 sendMessage.setText((String) response);
